310d6d192d60d233ff1c5273bc6337b24f5e9b66
Damian Rios <yrios@wi-mobile.com>
2021-06-25 11:53:33 -0500

    implementacion de sentry

--- app_tareas/src/index.js
+++ app_tareas/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const Sentry = require("@sentry/node");
 const app = express();
 const db = require('./persistence');
 const getItems = require('./routes/getItems');
 const addItem = require('./routes/addItem');
 const updateItem = require('./routes/updateItem');
 const deleteItem = require('./routes/deleteItem');
+
+Sentry.init({
+    dsn: "https://91a9ce38ce9f4cd5b3bafc757639b611@o879927.ingest.sentry.io/5834091",
+});
 
 app.use(require('body-parser').json());
 app.use(express.static(__dirname + '/static'));
@@ -16,6 +21,7 @@ app.delete('/items/:id', deleteItem);
 
 db.init().then(() => {
     app.listen(3000, () => console.log('Listening on port 3000'));
+    Sentry.captureMessage(`inicio correcto`);
 }).catch((err) => {
     console.error(err);
     process.exit(1);
